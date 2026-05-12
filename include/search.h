6de33547ac1457c343817731e186b02ece7bd865
abnormallynormal <yichenxiao08@gmail.com>
2026-05-11 21:57:42 -0400

    iterative deepening

--- include/search.h
+++ include/search.h
@@ -12,3 +12,13 @@ int scoreMove(Board &board, Move m);
 int mvv_lva(Piece attack, Piece victim);
 
 int quiescence(Board &board, MoveGenerator &moveGen, int alpha, int beta, int ply = 0);
+
+inline Move iterativeDeepening(Board &board, MoveGenerator &moveGen, int maxDepth)
+{
+  Move bestMove;
+  for (int i = 1; i <= maxDepth; i++)
+  {
+    bestMove = rootNegamax(board, moveGen, i);
+  }
+  return bestMove;
+}
